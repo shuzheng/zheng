d69274d8b7552f16f92a03535f3998f63405ca23
shuzheng <469741414@qq.com>
2016-10-14 18:10:56 +0800

    add ehcache

--- cms/cms-service/src/test/java/com/zheng/cms/service/UserServiceTest.java
+++ cms/cms-service/src/test/java/com/zheng/cms/service/UserServiceTest.java
@@ -1,15 +1,13 @@
 package com.zheng.cms.service;
 
 import com.zheng.cms.model.User;
-import com.zheng.cms.service.UserService;
+import com.zheng.cms.model.UserVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-
-import com.zheng.cms.model.UserVO;
 
 /**
  * 测试service
