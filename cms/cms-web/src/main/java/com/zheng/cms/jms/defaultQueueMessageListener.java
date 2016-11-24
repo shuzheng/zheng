33578337e81d18175438491b0b29f34f960e31ac
shuzheng <469741414@qq.com>
2016-11-24 17:09:28 +0800

    增加mq消费者

---
+++ cms/cms-web/src/main/java/com/zheng/cms/jms/defaultQueueMessageListener.java
@@ -0,0 +1,38 @@
+package com.zheng.cms.jms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * MQ消费者
+ * Created by ZhangShuzheng on 2016/11/24.
+ */
+public class defaultQueueMessageListener implements MessageListener {
+
+	private static Logger _log = LoggerFactory.getLogger(defaultQueueMessageListener.class);
+
+	@Autowired
+	ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+	public void onMessage(final Message message) {
+		// 使用线程池多线程处理
+		threadPoolTaskExecutor.execute(new Runnable() {
+			public void run() {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					_log.info("defaultQueueMessageListener接收到：{}", textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
