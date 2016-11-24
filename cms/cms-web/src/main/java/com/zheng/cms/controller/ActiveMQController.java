0ba10eeea85d7e2f2c4aecd628dc791f64227b7a
shuzheng <469741414@qq.com>
2016-11-24 18:05:52 +0800

    注释activemq

--- cms/cms-web/src/main/java/com/zheng/cms/controller/ActiveMQController.java
+++ cms/cms-web/src/main/java/com/zheng/cms/controller/ActiveMQController.java
@@ -1,15 +1,9 @@
 package com.zheng.cms.controller;
 
-import com.zheng.common.util.JmsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.jms.Destination;
 
 /**
  * 消息队列controller
@@ -22,19 +16,19 @@ public class ActiveMQController extends BaseController {
 
 	private static Logger _log = LoggerFactory.getLogger(ActiveMQController.class);
 
-	@Autowired
-	JmsTemplate jmsTemplate;
-
-	@Autowired
-	Destination defaultQueueDestination;
-
-	@RequestMapping("/send")
-	@ResponseBody
-	public Object send() {
-		for (int i = 0; i < 1000; i ++) {
-			JmsUtil.sendMessage(jmsTemplate, defaultQueueDestination, "消息" + i);
-		}
-		return "success";
-	}
+//	@Autowired
+//	JmsTemplate jmsTemplate;
+//
+//	@Autowired
+//	Destination defaultQueueDestination;
+//
+//	@RequestMapping("/send")
+//	@ResponseBody
+//	public Object send() {
+//		for (int i = 0; i < 1000; i ++) {
+//			JmsUtil.sendMessage(jmsTemplate, defaultQueueDestination, "消息" + i);
+//		}
+//		return "success";
+//	}
 
 }
