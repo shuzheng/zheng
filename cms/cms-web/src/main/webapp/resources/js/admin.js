d38b3dfaaafe97b24f0631458a735ceba204a47d
shuzheng <469741414@qq.com>
2016-11-29 22:45:22 +0800

    更换字体图标

--- cms/cms-web/src/main/webapp/resources/js/admin.js
+++ cms/cms-web/src/main/webapp/resources/js/admin.js
@@ -2,5 +2,8 @@ $(function() {
 	// 侧边栏操作按钮
 	$('#guide').click(function() {
 		$(this).toggleClass('toggled');
+		$('#aside').toggleClass('toggled');
 	});
+	// Waves初始化
+	Waves.displayEffect();
 });
