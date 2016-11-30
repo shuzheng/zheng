136990a950131b67b3d597dff041b8baa0e9f495
shuzheng <469741414@qq.com>
2016-11-30 15:24:22 +0800

    美化滚动条

--- cms/cms-web/src/main/webapp/resources/js/admin.js
+++ cms/cms-web/src/main/webapp/resources/js/admin.js
@@ -2,8 +2,24 @@ $(function() {
 	// 侧边栏操作按钮
 	$('#guide').click(function() {
 		$(this).toggleClass('toggled');
-		$('#aside').toggleClass('toggled');
+		$('#sidebar').toggleClass('toggled');
+	});
+	// 侧边栏二级菜单
+	$('.sub-menu a').click(function() {
+		$(this).next().slideToggle(200);
+		$(this).parent().toggleClass('toggled');
 	});
 	// Waves初始化
 	Waves.displayEffect();
+	// 滚动条初始化
+	$('#sidebar').mCustomScrollbar({
+		theme: 'minimal-dark',
+		scrollInertia: 100,
+		axis: 'yx',
+		mouseWheel: {
+			enable: true,
+			axis: 'y',
+			preventDefault: true
+		}
+	});
 });
