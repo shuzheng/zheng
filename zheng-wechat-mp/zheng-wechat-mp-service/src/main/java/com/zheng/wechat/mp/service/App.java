ad39b7d4d171894d5e5a0fa660ed904a0beb6735
shuzheng <469741414@qq.com>
2016-12-01 16:13:31 +0800

    add zheng-wechat-mp

---
+++ zheng-wechat-mp/zheng-wechat-mp-service/src/main/java/com/zheng/wechat/mp/service/App.java
@@ -0,0 +1,13 @@
+package com.zheng.wechat.mp.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
