32d83d7024c11be21ed77fab6bb5db214047acda
shuzheng <469741414@qq.com>
2016-12-01 16:03:59 +0800

    add zheng-ucenter

---
+++ zheng-ucenter/zheng-ucenter-dao/src/main/java/com/zheng/ucenter/dao/App.java
@@ -0,0 +1,13 @@
+package com.zheng.ucenter.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
