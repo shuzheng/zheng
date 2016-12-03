df0ae2dd015742197ba9a89ea8ff28e003df695f
shuzheng <469741414@qq.com>
2016-12-04 00:45:11 +0800

    增加操作选项卡事件

--- zheng-cms/zheng-cms-web/src/main/webapp/resources/js/admin.js
+++ zheng-cms/zheng-cms-web/src/main/webapp/resources/js/admin.js
@@ -28,8 +28,33 @@ $(function() {
 		}
 	});
 	// 选项卡
-	$('.content_tab li').click(function() {
+	$(document).on('click', '.content_tab li', function() {
 		$('.content_tab li').removeClass('cur');
 		$(this).addClass('cur');
 	});
+	// 控制选项卡滚动位置 
+	$('.tab_left>a').click(function() {
+		$('.content_tab>ul').animate({scrollLeft: $('.content_tab>ul').scrollLeft() - 300}, 200);
+		if ($('.content_tab>ul').scrollLeft() == 0) {
+			$('.tab_left>a').removeClass('active');
+		} else {
+			$('.tab_left>a').addClass('active');
+		}
+	});
+	$('.tab_right>a').click(function() {
+		$('.content_tab>ul').animate({scrollLeft: $('.content_tab>ul').scrollLeft() + 300}, 200);
+		if ($('.content_tab>ul').scrollLeft() == 0) {
+			$('.tab_left>a').removeClass('active');
+		} else {
+			$('.tab_left>a').addClass('active');
+		}
+	});
+	// 初始化
 });
+var tab = {
+	addTab: function(title, url) {
+		$('.content_tab li').removeClass('cur');
+		var tab = '<li class="cur"><a class="waves-effect waves-light" href="javascript:;">' + title + '</a></li>';
+		$('.content_tab>ul').append(tab);
+	}
+}
