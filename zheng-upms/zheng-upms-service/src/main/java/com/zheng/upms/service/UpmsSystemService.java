dad7e4d2dce49fb33d73d2c6b722675d3a39684c
shuzheng <469741414@qq.com>
2016-12-18 21:06:01 +0800

    增加系统service

---
+++ zheng-upms/zheng-upms-service/src/main/java/com/zheng/upms/service/UpmsSystemService.java
@@ -0,0 +1,14 @@
+package com.zheng.upms.service;
+
+import com.zheng.upms.dao.mapper.UpmsSystemMapper;
+
+/**
+ * 系统service接口
+ * Created by shuzheng on 2016/12/18.
+ */
+public interface UpmsSystemService extends BaseService<UpmsSystemMapper> {
+
+    // 批量删除
+    int deleteByPrimaryKeys(String ids);
+
+}
