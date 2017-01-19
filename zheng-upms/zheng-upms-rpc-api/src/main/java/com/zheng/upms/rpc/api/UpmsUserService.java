e62406ef5bd1be2d408890032a7abf353147ddb1
shuzheng <469741414@qq.com>
2017-01-19 22:08:25 +0800

    实现UpmsUserService

---
+++ zheng-upms/zheng-upms-rpc-api/src/main/java/com/zheng/upms/rpc/api/UpmsUserService.java
@@ -0,0 +1,16 @@
+package com.zheng.upms.rpc.api;
+
+import com.zheng.common.base.BaseService;
+import com.zheng.upms.dao.model.UpmsSystem;
+import com.zheng.upms.dao.model.UpmsSystemExample;
+import com.zheng.upms.dao.model.UpmsUser;
+import com.zheng.upms.dao.model.UpmsUserExample;
+
+/**
+ * 用户service接口
+ * Created by shuzheng on 2016/01/19.
+ */
+public interface UpmsUserService extends BaseService<UpmsUser, UpmsUserExample> {
+
+
+}
