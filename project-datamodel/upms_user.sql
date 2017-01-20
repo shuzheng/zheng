8d663fcc8454f1fb2df04a948615c6c326f3d53e
shuzheng <469741414@qq.com>
2017-01-20 15:47:57 +0800

    更新测试用户帐号密码

--- project-datamodel/upms_user.sql
+++ project-datamodel/upms_user.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50621
+Source Server Version : 50528
 Source Host           : localhost:3306
 Source Database       : zheng
 
 Target Server Type    : MYSQL
-Target Server Version : 50621
+Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2017-01-19 21:55:06
+Date: 2017-01-20 15:47:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -37,4 +37,4 @@ CREATE TABLE `upms_user` (
 -- ----------------------------
 -- Records of upms_user
 -- ----------------------------
-INSERT INTO `upms_user` VALUES ('1', '1', 'admin', '5E9167785DCF89329449EEFF9054099A', '827CCB0EEA8A706C4C34A16891F84E7B', '管理员', null, null, null, '1', '1');
+INSERT INTO `upms_user` VALUES ('1', '1', 'admin', '3038D9CB63B3152A79B8153FB06C02F7', '66f1b370c660445a8657bf8bf1794486', '管理员', null, null, null, '1', '1');
