f7672e3e025fa29be53c6e11753626c56456eb6d
hexl21 <hexl21@163.com>
2017-01-22 18:05:27 +0800

    增加全屏功能（天元）

--- zheng-admin/src/js/admin.js
+++ zheng-admin/src/js/admin.js
@@ -220,3 +220,14 @@ function initScrollState() {
 		$('.tab_right>a').addClass('active');
 	}
 }
+function fullPage() {
+	if ($.util.supportsFullScreen) {
+		if ($.util.isFullScreen()) {
+			$.util.cancelFullScreen();
+		} else {
+			$.util.requestFullScreen();
+		}
+	} else {
+		alert("当前浏览器不支持全屏 API，请更换至最新的 Chrome/Firefox/Safari 浏览器或通过 F11 快捷键进行操作。");
+	}
+}
