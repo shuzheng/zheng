beb85a85503bd802c177d5853615e4ff41e4335e
shuzheng <469741414@qq.com>
2017-02-04 17:53:17 +0800

    配置文件中的密码使用工具类DESUtil.java加密

--- zheng-common/src/main/java/com/zheng/common/util/RedisUtil.java
+++ zheng-common/src/main/java/com/zheng/common/util/RedisUtil.java
@@ -15,165 +15,165 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class RedisUtil {
 
-    protected static ReentrantLock lockPool = new ReentrantLock();
-    protected static ReentrantLock lockJedis = new ReentrantLock();
+	protected static ReentrantLock lockPool = new ReentrantLock();
+	protected static ReentrantLock lockJedis = new ReentrantLock();
 
-    private static Logger logger = LoggerFactory.getLogger(RedisUtil.class);
+	private static Logger logger = LoggerFactory.getLogger(RedisUtil.class);
 
-    //Redis服务器IP
-    private static String IP = PropertiesFileUtil.getInstance("redis").get("ip");
+	// Redis服务器IP
+	private static String IP = PropertiesFileUtil.getInstance("redis").get("master.redis.ip");
 
-    //Redis的端口号
-    private static int PORT = PropertiesFileUtil.getInstance("redis").getInt("port");
+	// Redis的端口号
+	private static int PORT = PropertiesFileUtil.getInstance("redis").getInt("master.redis.port");
 
-    //访问密码
-    private static String PASSWORD = PropertiesFileUtil.getInstance("redis").get("password");
-    //可用连接实例的最大数目，默认值为8；
-    //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-    private static int MAX_ACTIVE = PropertiesFileUtil.getInstance("redis").getInt("max_active");
+	// 访问密码
+	private static String PASSWORD = DESUtil.getDecryptString(PropertiesFileUtil.getInstance("redis").get("master.redis.password"));
+	// 可用连接实例的最大数目，默认值为8；
+	// 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+	private static int MAX_ACTIVE = PropertiesFileUtil.getInstance("redis").getInt("master.redis.max_active");
 
-    //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
-    private static int MAX_IDLE = PropertiesFileUtil.getInstance("redis").getInt("max_idle");
+	// 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+	private static int MAX_IDLE = PropertiesFileUtil.getInstance("redis").getInt("master.redis.max_idle");
 
-    //等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
-    private static int MAX_WAIT = PropertiesFileUtil.getInstance("redis").getInt("max_wait");
+	// 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
+	private static int MAX_WAIT = PropertiesFileUtil.getInstance("redis").getInt("master.redis.max_wait");
 
-    //超时时间
-    private static int TIMEOUT = PropertiesFileUtil.getInstance("redis").getInt("timeout");
+	// 超时时间
+	private static int TIMEOUT = PropertiesFileUtil.getInstance("redis").getInt("master.redis.timeout");
 
-    //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
-    private static boolean TEST_ON_BORROW = true;
+	// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+	private static boolean TEST_ON_BORROW = true;
 
-    private static JedisPool jedisPool = null;
+	private static JedisPool jedisPool = null;
 
-    /**
-     * redis过期时间,以秒为单位
-     */
-    public final static int EXRP_HOUR = 60 * 60;            //一小时
-    public final static int EXRP_DAY = 60 * 60 * 24;        //一天
-    public final static int EXRP_MONTH = 60 * 60 * 24 * 30;    //一个月
+	/**
+	 * redis过期时间,以秒为单位
+	 */
+	public final static int EXRP_HOUR = 60 * 60;            //一小时
+	public final static int EXRP_DAY = 60 * 60 * 24;        //一天
+	public final static int EXRP_MONTH = 60 * 60 * 24 * 30;    //一个月
 
-    /**
-     * 初始化Redis连接池
-     */
-    private static void initialPool() {
-        try {
-            JedisPoolConfig config = new JedisPoolConfig();
-            config.setMaxTotal(MAX_ACTIVE);
-            config.setMaxIdle(MAX_IDLE);
-            config.setMaxWaitMillis(MAX_WAIT);
-            config.setTestOnBorrow(TEST_ON_BORROW);
-            //jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT, PASSWORD);
-            jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT);
-        } catch (Exception e) {
-            logger.error("First create JedisPool error : " + e);
-            try {
-                //如果第一个IP异常，则访问第二个IP
-                JedisPoolConfig config = new JedisPoolConfig();
-                config.setMaxTotal(MAX_ACTIVE);
-                config.setMaxIdle(MAX_IDLE);
-                config.setMaxWaitMillis(MAX_WAIT);
-                config.setTestOnBorrow(TEST_ON_BORROW);
-                //jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT, PASSWORD);
-                jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT);
-            } catch (Exception e2) {
-                logger.error("Second create JedisPool error : " + e2);
-            }
-        }
-    }
+	/**
+	 * 初始化Redis连接池
+	 */
+	private static void initialPool() {
+		try {
+			JedisPoolConfig config = new JedisPoolConfig();
+			config.setMaxTotal(MAX_ACTIVE);
+			config.setMaxIdle(MAX_IDLE);
+			config.setMaxWaitMillis(MAX_WAIT);
+			config.setTestOnBorrow(TEST_ON_BORROW);
+			//jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT, PASSWORD);
+			jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT);
+		} catch (Exception e) {
+			logger.error("First create JedisPool error : " + e);
+			try {
+				//如果第一个IP异常，则访问第二个IP
+				JedisPoolConfig config = new JedisPoolConfig();
+				config.setMaxTotal(MAX_ACTIVE);
+				config.setMaxIdle(MAX_IDLE);
+				config.setMaxWaitMillis(MAX_WAIT);
+				config.setTestOnBorrow(TEST_ON_BORROW);
+				//jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT, PASSWORD);
+				jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT);
+			} catch (Exception e2) {
+				logger.error("Second create JedisPool error : " + e2);
+			}
+		}
+	}
 
-    /**
-     * 在多线程环境同步初始化
-     */
-    private static synchronized void poolInit() {
-        if (jedisPool == null) {
-            initialPool();
-        }
-    }
+	/**
+	 * 在多线程环境同步初始化
+	 */
+	private static synchronized void poolInit() {
+		if (jedisPool == null) {
+			initialPool();
+		}
+	}
 
 
-    /**
-     * 同步获取Jedis实例
-     * @return Jedis
-     */
-    public synchronized static Jedis getJedis() {
-        if (jedisPool == null) {
-            poolInit();
-        }
-        Jedis jedis = null;
-        try {
-            if (jedisPool != null) {
-                jedis = jedisPool.getResource();
-            }
-        } catch (Exception e) {
-            logger.error("Get jedis error : " + e);
-        } finally {
-            returnResource(jedis);
-        }
-        return jedis;
-    }
+	/**
+	 * 同步获取Jedis实例
+	 * @return Jedis
+	 */
+	public synchronized static Jedis getJedis() {
+		if (jedisPool == null) {
+			poolInit();
+		}
+		Jedis jedis = null;
+		try {
+			if (jedisPool != null) {
+				jedis = jedisPool.getResource();
+			}
+		} catch (Exception e) {
+			logger.error("Get jedis error : " + e);
+		} finally {
+			returnResource(jedis);
+		}
+		return jedis;
+	}
 
-    /**
-     * 释放jedis资源
-     * @param jedis
-     */
-    public static void returnResource(final Jedis jedis) {
-        if (jedis != null && jedisPool != null) {
-            jedisPool.returnResource(jedis);
-        }
-    }
+	/**
+	 * 释放jedis资源
+	 * @param jedis
+	 */
+	public static void returnResource(final Jedis jedis) {
+		if (jedis != null && jedisPool != null) {
+			jedisPool.returnResource(jedis);
+		}
+	}
 
-    /**
-     * 设置 String
-     * @param key
-     * @param value
-     */
-    public synchronized static void set(String key, String value) {
-        try {
-            value = StringUtils.isEmpty(value) ? "" : value;
-            getJedis().set(key, value);
-        } catch (Exception e) {
-            logger.error("Set key error : " + e);
-        }
-    }
+	/**
+	 * 设置 String
+	 * @param key
+	 * @param value
+	 */
+	public synchronized static void set(String key, String value) {
+		try {
+			value = StringUtils.isEmpty(value) ? "" : value;
+			getJedis().set(key, value);
+		} catch (Exception e) {
+			logger.error("Set key error : " + e);
+		}
+	}
 
-    /**
-     * 设置 过期时间
-     * @param key
-     * @param value
-     * @param seconds 以秒为单位
-     */
-    public synchronized static void set(String key, String value, int seconds) {
-        try {
-            value = StringUtils.isEmpty(value) ? "" : value;
-            getJedis().setex(key, seconds, value);
-        } catch (Exception e) {
-            logger.error("Set keyex error : " + e);
-        }
-    }
+	/**
+	 * 设置 过期时间
+	 * @param key
+	 * @param value
+	 * @param seconds 以秒为单位
+	 */
+	public synchronized static void set(String key, String value, int seconds) {
+		try {
+			value = StringUtils.isEmpty(value) ? "" : value;
+			getJedis().setex(key, seconds, value);
+		} catch (Exception e) {
+			logger.error("Set keyex error : " + e);
+		}
+	}
 
-    /**
-     * 获取String值
-     * @param key
-     * @return value
-     */
-    public synchronized static String get(String key) {
-        if (getJedis() == null || !getJedis().exists(key)) {
-            return null;
-        }
-        return getJedis().get(key);
-    }
+	/**
+	 * 获取String值
+	 * @param key
+	 * @return value
+	 */
+	public synchronized static String get(String key) {
+		if (getJedis() == null || !getJedis().exists(key)) {
+			return null;
+		}
+		return getJedis().get(key);
+	}
 
-    /**
-     * 删除值
-     * @param key
-     */
-    public synchronized static void remove(String key) {
-        try {
-            getJedis().del(key);
-        } catch (Exception e) {
-            logger.error("Remove keyex error : " + e);
-        }
-    }
+	/**
+	 * 删除值
+	 * @param key
+	 */
+	public synchronized static void remove(String key) {
+		try {
+			getJedis().del(key);
+		} catch (Exception e) {
+			logger.error("Remove keyex error : " + e);
+		}
+	}
 
 }
