a554ccdda9a15ab008ac0e41da7efeada5bf0c80
shuzheng <469741414@qq.com>
2017-02-11 14:52:12 +0800

    更新sql

--- project-datamodel/zheng.sql
+++ project-datamodel/zheng.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2017-02-10 20:53:21
+Date: 2017-02-11 12:32:23
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -519,7 +519,7 @@ CREATE TABLE `upms_user` (
 -- ----------------------------
 -- Records of upms_user
 -- ----------------------------
-INSERT INTO `upms_user` VALUES ('1', 'admin', '3038D9CB63B3152A79B8153FB06C02F7', '66f1b370c660445a8657bf8bf1794486', '管理员', null, null, null, null, '0', '1');
+INSERT INTO `upms_user` VALUES ('1', 'admin', '3038D9CB63B3152A79B8153FB06C02F7', '66f1b370c660445a8657bf8bf1794486', '管理员', '/resources/zheng-admin/images/avatar.jpg', null, null, null, '0', '1');
 
 -- ----------------------------
 -- Table structure for upms_user_organization
