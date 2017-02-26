453827fd2def82f9694bf9475964576c19ce408a
shuzheng <469741414@qq.com>
2017-02-26 21:49:09 +0800

    增加用户组织关联表rpc服务

---
+++ zheng-upms/zheng-upms-rpc-api/src/main/java/com/zheng/upms/rpc/api/UpmsUserOrganizationService.java
@@ -0,0 +1,14 @@
+package com.zheng.upms.rpc.api;
+
+import com.zheng.common.base.BaseService;
+import com.zheng.upms.dao.model.UpmsUserOrganization;
+import com.zheng.upms.dao.model.UpmsUserOrganizationExample;
+
+/**
+ * 用户组织service接口
+ * Created by shuzheng on 2017/2/26.
+ */
+public interface UpmsUserOrganizationService extends BaseService<UpmsUserOrganization, UpmsUserOrganizationExample> {
+
+
+}
---
+++ zheng-upms/zheng-upms-rpc-api/src/main/java/com/zheng/upms/rpc/api/UpmsUserOrganizationServiceMock.java
@@ -0,0 +1,14 @@
+package com.zheng.upms.rpc.api;
+
+import com.zheng.common.base.BaseServiceMock;
+import com.zheng.upms.dao.mapper.UpmsUserOrganizationMapper;
+import com.zheng.upms.dao.model.UpmsUserOrganization;
+import com.zheng.upms.dao.model.UpmsUserOrganizationExample;
+
+/**
+ * 降级实现UpmsUserOrganizationService接口
+ * Created by shuzheng on 2017/2/14.
+ */
+public class UpmsUserOrganizationServiceMock extends BaseServiceMock<UpmsUserOrganizationMapper, UpmsUserOrganization, UpmsUserOrganizationExample> implements UpmsUserOrganizationService {
+
+}
