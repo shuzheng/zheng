1cebbe3e880b7f0a1e9fa0b55aebc24e7b536065
kayuu0810@163.com <mhy601212>
2017-03-03 23:09:21 +0800

    cms系统跨域请求生成菜单完善

--- zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/dao/mapper/UpmsApiMapper.java
+++ zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/dao/mapper/UpmsApiMapper.java
@@ -19,7 +19,7 @@ public interface UpmsApiMapper {
 
 	// 根据用户id获取所属的角色
 	List<UpmsRole> selectUpmsRoleByUpmsUserId(Integer upmsUserId);
-
+   // 根据用户和系统ID获取所拥有的权限
 	List<UpmsPermission> selectUpmsPermissionByUpmsUserIdAndSystemId(Map<String, Integer> params);
 
 
