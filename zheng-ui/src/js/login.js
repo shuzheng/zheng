2c0cb9bc00eff779e5843e19e9a3c8a4be05baa7
shuzheng <469741414@qq.com>
2017-03-05 19:47:06 +0800

    登录页面js，移到外部js

--- zheng-ui/src/js/login.js
+++ zheng-ui/src/js/login.js
@@ -8,3 +8,48 @@ $(function() {
 		$(this).parent().removeClass('fg-toggled');
 	});
 });
+Checkbix.init();
+$(function() {
+	// 点击登录按钮
+	$('#login-bt').click(function() {
+		login();
+	});
+	// 回车事件
+	$('#username, #password').keypress(function (event) {
+		if (13 == event.keyCode) {
+			login();
+		}
+	});
+});
+// 登录
+function login() {
+	$.ajax({
+		url: '${basePath}/sso/login',
+		type: 'POST',
+		data: {
+			username: $('#username').val(),
+			password: $('#password').val(),
+			rememberMe: $('#rememberMe').is(':checked'),
+			backurl: '${param.backurl}'
+		},
+		beforeSend: function() {
+
+		},
+		success: function(json){
+			if (json.code == 1) {
+				location.href = json.data;
+			} else {
+				alert(json.data);
+				if (10101 == json.code) {
+					$('#username').focus();
+				}
+				if (10102 == json.code) {
+					$('#password').focus();
+				}
+			}
+		},
+		error: function(error){
+			console.log(error);
+		}
+	});
+}
