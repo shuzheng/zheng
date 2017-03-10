b953631f5bc7bdb11b486bc9e7af10dc28e3f166
shuzheng <469741414@qq.com>
2017-03-10 23:13:13 +0800

    修正默认主题不显示问题

--- zheng-ui/src/js/admin.js
+++ zheng-ui/src/js/admin.js
@@ -44,7 +44,7 @@ $(function() {
 	});
 	// 显示cookie菜单
 	var systemid = $.cookie('zheng-upms-systemid') || 1;
-	var systemname = $.cookie('zheng-upms-systemname') || 'zheng-upms';
+	var systemname = $.cookie('zheng-upms-systemname') || 'zheng-upms-server';
 	var systemtitle = $.cookie('zheng-upms-systemtitle') || '权限管理系统';
 	$('.system_menus').hide(0, function () {
 		$('.system_' + systemid).show();
