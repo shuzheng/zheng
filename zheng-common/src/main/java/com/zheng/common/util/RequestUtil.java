8c58c7bc0bcb775168cb006a3b6aed7359f22430
shuzheng <469741414@qq.com>
2017-03-14 16:25:47 +0800

    更新RequestUtil工具类

--- zheng-common/src/main/java/com/zheng/common/util/RequestUtil.java
+++ zheng-common/src/main/java/com/zheng/common/util/RequestUtil.java
@@ -32,4 +32,25 @@ public class RequestUtil {
 		return queryString;
 	}
 
+	/**
+	 * 获取请求basePath
+	 * @param request
+	 * @return
+	 */
+	public String getBasePath(HttpServletRequest request) {
+		StringBuffer basePath = new StringBuffer();
+		String scheme = request.getScheme();
+		String domain = request.getServerName();
+		int port = request.getServerPort();
+		basePath.append(scheme);
+		basePath.append("://");
+		basePath.append(domain);
+		if("http".equalsIgnoreCase(scheme) && 80 != port) {
+			basePath.append(":").append(String.valueOf(port));
+		} else if("https".equalsIgnoreCase(scheme) && port != 443) {
+			basePath.append(":").append(String.valueOf(port));
+		}
+		return basePath.toString();
+	}
+	
 }
