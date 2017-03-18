3fba02352786e605fe320514c3b258a080fa7b61
shuzheng <469741414@qq.com>
2017-03-19 02:04:59 +0800

    首页动态菜单

--- zheng-cms/zheng-cms-web/src/main/java/com/zheng/cms/web/controller/IndexController.java
+++ zheng-cms/zheng-cms-web/src/main/java/com/zheng/cms/web/controller/IndexController.java
@@ -1,11 +1,18 @@
 package com.zheng.cms.web.controller;
 
+import com.zheng.cms.dao.model.CmsMenu;
+import com.zheng.cms.dao.model.CmsMenuExample;
+import com.zheng.cms.rpc.api.CmsMenuService;
 import com.zheng.common.base.BaseController;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * 首页控制器
@@ -16,8 +23,16 @@ public class IndexController extends BaseController {
 
 	private static Logger _log = LoggerFactory.getLogger(IndexController.class);
 
+	@Autowired
+	private CmsMenuService cmsMenuService;
+
 	@RequestMapping(value = {"", "/index"})
-	public String index() {
+	public String index(Model model) {
+		// 菜单
+		CmsMenuExample cmsMenuExample = new CmsMenuExample();
+		cmsMenuExample.setOrderByClause("orders asc");
+		List<CmsMenu> menus = cmsMenuService.selectByExample(cmsMenuExample);
+		model.addAttribute("menus", menus);
 		return "/index";
 	}
 
---
+++ zheng-cms/zheng-cms-web/src/main/webapp/resources/inc/head.jsp
@@ -0,0 +1,7 @@
+﻿<%@ page contentType="text/html; charset=utf-8"%>
+<%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<c:set var="basePath" value="${pageContext.request.contextPath}"/>
