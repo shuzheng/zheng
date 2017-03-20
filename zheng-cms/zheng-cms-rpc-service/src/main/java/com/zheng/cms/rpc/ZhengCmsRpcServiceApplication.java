31f5e432d49548d2bf0747edd1713ce6e0669a39
shuzheng <469741414@qq.com>
2017-03-20 18:01:48 +0800

    更新rpc-service启动类路径

--- zheng-cms/zheng-cms-rpc-service/src/main/java/com/zheng/cms/rpc/ZhengCmsRpcServiceApplication.java
+++ zheng-cms/zheng-cms-rpc-service/src/main/java/com/zheng/cms/rpc/ZhengCmsRpcServiceApplication.java
@@ -1,4 +1,4 @@
-package com.zheng.cms.rpc.service;
+package com.zheng.cms.rpc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/rpc/ZhengUpmsRpcServiceApplication.java
+++ zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/rpc/ZhengUpmsRpcServiceApplication.java
@@ -1,4 +1,4 @@
-package com.zheng.upms.rpc.service;
+package com.zheng.upms.rpc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
