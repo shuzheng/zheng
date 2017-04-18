4e297ea5849a568e45ed397df310e7b0c0ab48f2
shuzheng <469741414@qq.com>
2017-04-18 22:44:12 +0800

    增加web演示

--- zheng-oss/zheng-oss-web/src/main/java/com/zheng/oss/web/controller/DemoController.java
+++ zheng-oss/zheng-oss-web/src/main/java/com/zheng/oss/web/controller/DemoController.java
@@ -1,7 +1,8 @@
-﻿package com.zheng.oss.web.controller;
+package com.zheng.oss.web.controller;
 
 import com.aliyun.oss.OSSClient;
-import org.springframework.beans.factory.annotation.Value;
+import com.zheng.oss.rpc.api.OssApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +12,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.ByteArrayInputStream;
 
 /**
- * Created by shuzheng on 2016/11/12.
+ * Created by shuzheng on 2017/4/18.
  */
 @Controller
-@RequestMapping("/app")
-public class AppController {
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Autowired
+    private OssApiService ossApiService;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     @ResponseBody
     public String index(ModelMap modelMap) {
         // endpoint以杭州为例，其它region请按实际情况填写
-        String endpoint = "";
+        String endpoint = "oss-cn-shanghai.aliyuncs.com";
         // accessKey请登录https://ak-console.aliyun.com/#/查看
-        String accessKeyId = "";
-        String accessKeySecret = "";
+        String accessKeyId = "LTAIf5dDIthJN3h0";
+        String accessKeySecret = "2IZVHc1Qzxul8rC0ZQGKCEjjnpvm5d";
 
         // 创建OSSClient实例
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
@@ -35,6 +39,7 @@ public class AppController {
 
         // 关闭ossClient
         ossClient.shutdown();
+
         return "success";
     }
 
