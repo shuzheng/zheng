c49a349602a294dc2e2f26a3f08b86b8ceee46df
shuzheng <469741414@qq.com>
2017-04-18 22:42:23 +0800

    修正oss的rpc服务启动类

--- zheng-oss/zheng-oss-rpc-service/src/main/java/com/zheng/oss/rpc/ZhengOssRpcServiceApplication.java
+++ zheng-oss/zheng-oss-rpc-service/src/main/java/com/zheng/oss/rpc/ZhengOssRpcServiceApplication.java
@@ -14,7 +14,7 @@ public class ZhengOssRpcServiceApplication {
 
 	public static void main(String[] args) {
 		_log.info(">>>>> zheng-oss-rpc-service 正在启动 <<<<<");
-		new ClassPathXmlApplicationContext("classpath*:applicationContext*.xml");
+		new ClassPathXmlApplicationContext("classpath:META-INF/spring/*.xml");
 		_log.info(">>>>> zheng-oss-rpc-service 启动完成 <<<<<");
 	}
 
