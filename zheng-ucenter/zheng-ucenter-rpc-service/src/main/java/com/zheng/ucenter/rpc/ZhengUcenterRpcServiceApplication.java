320d85ee795c3c6ef82cc2dd712283886723bf57
shuzheng <469741414@qq.com>
2018-11-04 16:50:13 +0800

    改为Dubbo Main启动方式

--- zheng-ucenter/zheng-ucenter-rpc-service/src/main/java/com/zheng/ucenter/rpc/ZhengUcenterRpcServiceApplication.java
+++ zheng-ucenter/zheng-ucenter-rpc-service/src/main/java/com/zheng/ucenter/rpc/ZhengUcenterRpcServiceApplication.java
@@ -1,21 +1,13 @@
 package com.zheng.ucenter.rpc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * 服务启动类
  * Created by ZhangShuzheng on 2017/6/19.
  */
 public class ZhengUcenterRpcServiceApplication {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ZhengUcenterRpcServiceApplication.class);
-
 	public static void main(String[] args) {
-		LOGGER.info(">>>>> zheng-ucenter-rpc-service 正在启动 <<<<<");
-		new ClassPathXmlApplicationContext("classpath:META-INF/spring/*.xml");
-		LOGGER.info(">>>>> zheng-ucenter-rpc-service 启动完成 <<<<<");
+		com.alibaba.dubbo.container.Main.main(args);
 	}
 
 }
